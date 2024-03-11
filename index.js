2a37c7327bf82f5fd65be9e10836294305681198
vanshdengani <130483965+vanshdengani@users.noreply.github.com>
2024-03-11 15:13:42 +0530

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var numberofbuttons=document.querySelectorAll(".drum").length;
+document.addEventListener("keypress", function(event){
+    makesound(event.key);     
+
+  });
+for(var i=0;i<=numberofbuttons;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var innerbutton=this.innerHTML;
+    makesound(innerbutton);
+    });
+
+    
+    
+   
+            }
+            function makesound(key){
+                switch(key){
+                    case "w":
+                        var audio = new Audio('./tom-1.mp3');
+                        audio.play();
+                        break;
+                        
+                        case "a":
+                            var audio = new Audio('./tom-2.mp3');
+                            audio.play();
+                            break;
+                            
+                        case "s":
+                            var audio = new Audio('./tom-3.mp3');
+                            audio.play();
+                            break;
+                            
+                        case "d":
+                            var audio = new Audio('./tom-4.mp3');
+                            audio.play();
+                            break;
+                        
+                        case "j":
+                            var audio = new Audio('./snare.mp3');
+                            audio.play();
+                            break;
+                        
+                        case "k":
+                            var audio = new Audio('./crash.mp3');
+                            audio.play();
+                            break;
+                        
+                        case "l":
+                            var  audio = new Audio('./kick-bass.mp3');
+                            audio.play();
+                            break;
+                        
+                        default:
+                           break;
+            
+        }    
+        
+    }       
